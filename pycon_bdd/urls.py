6a94feb0c8fff6d2dfc4c6e51b3a59a53a3412c3
mixxorz <mixxorz@gmail.com>
2015-07-04 22:20:33 +0800

    Added basic template view for the app

--- pycon_bdd/urls.py
+++ pycon_bdd/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'todo_app.views.todo_view', name='todo_view')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class TodoView(TemplateView):
+    template_name = 'todo_list.html'
+
+todo_view = TodoView.as_view()
