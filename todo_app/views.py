8c2d139caa9254a60605549947f678957226fa46
mixxorz <mixxorz@gmail.com>
2015-07-05 00:02:20 +0800

    Implemented add todo items

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,17 @@
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.views.generic import ListView
+
+from .models import TodoItem
 
 
-class TodoView(TemplateView):
+class TodoView(ListView):
+    context_object_name = 'todo_items'
+    model = TodoItem
     template_name = 'todo_list.html'
+
+    def post(self, request, *args, **kwargs):
+        TodoItem.objects.create(text=request.POST['todo_item'])
+        return redirect(reverse('todo_view'))
 
 todo_view = TodoView.as_view()
