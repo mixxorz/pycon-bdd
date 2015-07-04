6b246ed1d1cbfb75eeb300d61d70c6766880f925
mixxorz <mixxorz@gmail.com>
2015-07-05 00:34:33 +0800

    Implemented delete test

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -17,9 +17,29 @@ def no_todo_items(context):
 @given(u'there is one todo item')
 def one_todo_items(context):
     TodoItem.objects.create(text='First todo item')
+    context.browser.reload()
 
 
 @when(u'I add "{text}"')
-def buy_pycon_tickets(context, text):
+def add_todo_item(context, text):
     context.browser.fill('todo_item', text)
     context.browser.find_by_id('btnSubmit').first.click()
+
+
+@given(u'the following todo items')
+def table_todo_items(context):
+    for row in context.table:
+        TodoItem.objects.create(text=row['todo_items'])
+
+    context.browser.reload()
+
+
+@when(u'I delete "{text}"')
+def delete_todo_item(context, text):
+    context.browser.find_by_css('[data-text="%s"]' % text).first.click()
+
+
+@then(u'I should not see "{text}"')
+def step_impl(context, text):
+    context.test.assertFalse(context.browser.is_text_present(text),
+                             '"%s" was found' % text)
