b774051a55e7c5718af3aa5acc14fcd09e74ff9f
mixxorz <mixxorz@gmail.com>
2015-07-04 23:35:50 +0800

    Scaffolded the steps

---
+++ features/steps/steps.py
@@ -0,0 +1,16 @@
+from behave import given, when, then
+
+
+@then(u'I should see "1. Buy PyCon tickets"')
+def i_should_see_1_buy_pycon_tickets(context):
+    raise NotImplementedError(u'STEP: I should see "1. Buy PyCon tickets"')
+
+
+@given(u'there are no todo items')
+def there_are_no_todo_items(context):
+    raise NotImplementedError(u'STEP: there are no todo items')
+
+
+@when(u'I add "Buy PyCon tickets"')
+def i_add_buy_pycon_tickets(context):
+    raise NotImplementedError(u'STEP: I add "Buy PyCon tickets"')
